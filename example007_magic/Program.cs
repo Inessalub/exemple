c1fba4731f5861e53e8932debecafa879255926c
unknown <inessalub@gmail.com>
2022-10-05 14:56:44 +0400

    add example 007

---
+++ example007_magic/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.SetCursorPosition (8, 4);
+Console.WriteLine ("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 15;
+int xc = 18;
+int yc = 15;
+
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine("+");
